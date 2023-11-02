9a27c3edbdafbbf496d2852cd52b7f1eabd2dc49
Viviane <viviane.sp13@gmail.com>
2023-11-01 23:14:10 -0300

    aulas anteriores

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const mysql = require("mysql2");
+
+const app = express();
+
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+app.use(express.static("public"));
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+const conn = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "nodemysql",
+  port: 3307,
+});
+
+conn.connect((error) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  console.log("Conectado ao MySQL.");
+
+  app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000.");
+  });
+});
