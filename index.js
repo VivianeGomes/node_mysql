97429633c5933d378dc9916bf7995014d3746475
Viviane <50222582+VivianeGomes@users.noreply.github.com>
2023-11-07 22:31:40 -0300

    Lendo dados de livros

--- index.js
+++ index.js
@@ -37,7 +37,17 @@ app.get("/register", (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  res.render("home");
+  const sql = 'SELECT * FROM books'
+
+  conn.query(sql, (error, data) => {
+    if (error) {
+      return console.log(error)
+    }
+    const books = data
+
+    res.render("home", {books});
+  })
+  
 });
 
 const conn = mysql.createConnection({
